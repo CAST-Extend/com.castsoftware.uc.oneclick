7aa14ffb76a2eb2904dd18bc60d869cf11fecdfa
Haig Bedros <h.bedros@castsoftware.com>
2023-05-17 14:17:02 -0400

    changes install.bat pre_install.bat unzipping the scripts folder

--- install.bat
+++ install.bat
@@ -1,56 +1,92 @@
 @echo off
-::validate input parameters
-set CODE_FOLDER=%1
-if "%CODE_FOLDER%x" == "x" GOTO missingCodeParam
+setlocal EnableDelayedExpansion
+
+:: OneClick base folder creation
+if exist "T:\CAST\CODE" (
+  echo OneClick base folder already exists in T:\
+  goto :OC1
+) else if exist "D:\CAST\CODE" (
+  echo OneClick base folder already exists in D:\
+  goto :OC2
+) else if exist "C:\CAST\CODE" (
+  echo OneClick base folder already exists in C:\
+  goto :OC3
+) else (
+  goto create_Path
+)
+
+:create_Path
+if exist "T:\" (
+  mkdir "T:\CAST\CODE"
+  echo Success: Created the OneClick base folder T:\CAST\CODE
+  goto :OC1
+) else if exist "D:\" (
+  mkdir "D:\CAST\CODE"
+  echo Success: Created the OneClick base folder D:\CAST\CODE
+  goto :OC2
+) else if exist "C:\" (
+  mkdir "C:\CAST\CODE"
+  echo Success: Created the OneClick base folder C:\CAST\CODE
+  goto :OC3
+) else (
+  echo Error: None of the required drives exist.
+  pause
+  exit
+)
+
+:: OneClick installation
+:OC1
+cd /d "T:\CAST\CODE"
+goto :start
+:OC2
+cd /d "D:\CAST\CODE"
+goto :start
+:OC3
+cd /d "C:\CAST\CODE"
+goto :start
+
+:: OneClick File Creation
+:: Validate input parameters
+:start
+set CODE_FOLDER=%1\CAST\CODE
+if "%CODE_FOLDER%x" == "x" goto missingCodeParam
 if not exist %CODE_FOLDER% goto missingCodeFolder
-
 echo Configure for installation
-:: Check for Python Installation
-
-python --version 2>NUL
-if errorlevel 1 (
-	py --version
-	if errorlevel 1 goto noPython
-	set EXEC=py
-) else set EXEC=python
 
 :: Check for Python Installation
+python -m ensurepip --default-pip
 call pip --version 2>NUL
+echo pip --version
+echo .
+echo .
+echo .
+echo .
 if errorlevel 1 goto noPip
 
 echo creating virutal enviroment 
 IF NOT EXIST "%CODE_FOLDER%\DELIVER" MKDIR "%CODE_FOLDER%\DELIVER"
 IF NOT EXIST "%CODE_FOLDER%\.oneClick" MKDIR "%CODE_FOLDER%\.oneClick"
-IF NOT EXIST "%CODE_FOLDER%\SCRIPTS" MKDIR "%CODE_FOLDER%\SCRIPTS"
-%EXEC% -m pip install --user --upgrade pip > NUL
+IF NOT EXIST "%CODE_FOLDER%\SCRIPTS" MKDIR "%CODE_FOLDER%\SCRIPTS
+python -m pip install --user --upgrade pip > NUL
 
-if not exist "%CODE_FOLDER%\.venv" %EXEC% -m venv "%CODE_FOLDER%\.venv"
+if not exist "%CODE_FOLDER%\.venv" python -m venv "%CODE_FOLDER%\.venv"
 if errorlevel 1 goto venvFail
 call %CODE_FOLDER%\.venv\scripts\activate.bat
 
+copy "%~dp0oneClick.bat" %CODE_FOLDER%
+xcopy "%~dp0scripts" "%CODE_FOLDER%\SCRIPTS\" /E /H /C /I /Y  
+
 echo adding Onclick package from PyPi
-pip install com.castsoftware.uc.oneClick
-Call :UnZipFile "%CODE_FOLDER%" "scripts.zip"
+pip install com.castsoftware.uc.oneclick==0.2.2.9
 
-call %CODE_FOLDER%\.venv\scripts\deactivate.bat
-
-exit /b 
-
-:UnZipFile <ExtractTo> <newzipfile>
-set vbs="%temp%\_.vbs"
-if exist %vbs% del /f /q %vbs%
->%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
->>%vbs% echo If NOT fso.FolderExists(%1) Then
->>%vbs% echo fso.CreateFolder(%1)
->>%vbs% echo End If
->>%vbs% echo set objShell = CreateObject("Shell.Application")
->>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
->>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
->>%vbs% echo Set fso = Nothing
->>%vbs% echo Set objShell = Nothing
-cscript //nologo %vbs%
-if exist %vbs% del /f /q %vbs%
-exit /b
+echo .
+echo .
+echo .
+echo Success: OneClick installation is setup!
+echo .
+echo .
+echo .
+goto config_setup
 
 :missingCodeParam
 echo Missing Code Folder Location parameter
@@ -58,10 +94,6 @@ goto usage
 
 :missingCodeFolder
 echo Destination folder "%CODE_FOLDER%" must already exist
-goto usage
-
-:noPython
-echo Python version 10.x must be installed to use OneClick
 goto usage
 
 :noPip
@@ -74,5 +106,14 @@ goto usage
 
 :usage
 echo install ^<Code Folder Location^>
-popd
+
+:config_setup
+echo OneClick global configuration setup...
+
+cd /d "%CODE_FOLDER%"
+python -m oneclick.setup config -b %CODE_FOLDER%
+
+call %CODE_FOLDER%\.venv\scripts\deactivate.bat
+
+pause
 exit /b
