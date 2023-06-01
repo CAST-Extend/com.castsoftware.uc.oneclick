7468531d464b17a68fb5a601ed46604c08ddec71
Nevin Kaplan <n.kaplan@castsoftware.com>
2023-06-01 08:59:12 -0400

    fix java path issue

--- src/oneclick/analysis/aip_analysis.py
+++ src/oneclick/analysis/aip_analysis.py
@@ -42,7 +42,7 @@ class AIPAnalysis(Analysis):
                 if config.blueprint:
                     blueprint='--blueprint'
 
-                args = [abspath(f'{java_home}/java.exe'),
+                args = [abspath(f'{java_home}/bin/java.exe'),
                         '-jar',config.console_cli,
                         'add',
                         '-n',appl,
