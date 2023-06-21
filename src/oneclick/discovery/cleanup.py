5b565f4ec9c6a806b7f1ea65928b7c32ddb132bf
Nevin Kaplan <n.kaplan@castsoftware.com>
2023-06-21 10:22:40 -0400

    Bug - Clean up process not deleting all it should

--- src/oneclick/discovery/cleanup.py
+++ src/oneclick/discovery/cleanup.py
@@ -7,6 +7,10 @@ from oneclick.config import Config
 from cast_common.logger import INFO
 from cast_common.util import create_folder
 
+from pandas import Series,DataFrame
+
+import re 
+
 #todo: review cleanup lists for aip and hl, do we need separate or can we keep it as one and run HL from AIP folder?
 
 class cleanUpAIP(SourceValidation):
@@ -23,8 +27,8 @@ class cleanUpAIP(SourceValidation):
     def run(cls,config:Config):
         cls._log.debug('Source Code cleanup is in progress')
         
-        output_path = f'{config.oneclick_work}/{config.project_name}/LOGS'    
-        #create_folder(output_path)
+        output_path = abspath(f'{config.oneclick_work}/LOGS/{config.project_name}')
+        create_folder(output_path)
 
         dir = config.base
         dateTimeObj=datetime.now()
@@ -47,54 +51,52 @@ class cleanUpAIP(SourceValidation):
             found = False
             for app in apps:
                 create_folder(f'{output_path}\\{app}')
-                clean_up_log_file= f"{output_path}\\{app}\\{cls.cleanup_file_prefix}{config.project_name}_{app}_deletedFiles_{file_suffix}.txt"
-                clean_up_log_folder= f"{output_path}\\{app}\\{cls.cleanup_file_prefix}{config.project_name}_{app}_deletedFolders_{file_suffix}.txt"
+                base = f'{output_path}\\{app}\\{cls.cleanup_file_prefix}{config.project_name}_{app}'
+                clean_up_log_file= f"{base}_deletedFiles_{file_suffix}.txt"
+                clean_up_log_folder= f"{base}_deletedFolders_{file_suffix}.txt"
 
-                app_folder = f'{config.work}\\{cls.cleanup_file_prefix}\\{config.project_name}\\{app}'
-
-                with open (clean_up_log_folder, 'a+') as file2: 
-                    s=''
-                        
-                    count=0
-                    for subdir, dirs, files in walk(app_folder):
-                            for dir in dirs:
-                                if dir in folder_list:
-                                    folder=join(subdir, dir)
-                                    rmtree(folder)
-                        
-                                    s=str(count)+") Removed folder -> "+folder
-                                    count+=1
-                                    file2.write(s)
-                                    file2.write('\n') 
-                    file2.close()
-                cls._log.info(f'Removed {count} unwanted folders from {app_folder}')
-                if count > 0:
-                    config.application[app]['aip']=""
-                    config._save()
-
+                app_folder = abspath(f'{config.work}\\{cls.cleanup_file_prefix}\\{config.project_name}\\{app}')
+                cls._log.info(f'Reviewing {app} ({app_folder})')
                 with open (clean_up_log_file, 'a+') as file1: 
-                    s=''
-                    count=0
-                    for subdir, dirs, files in walk(app_folder):
-                        for file in files:
-                            fileN=join(subdir, file) 
-                            for fileName in files_list:
-                                
-                                if fileN.endswith(fileName):
-                                    remove(fileN)
-                                
-                                    s=str(count)+") Removed file -> "+fileN
-                                    count+=1
-                                    file1.write(s)
-                                    file1.write('\n') 
-                    file1.close()
-                cls._log.info(f'Removed {count} unwanted files from {app_folder}')
-                if count > 0:
-                    config.application[app]['aip']=""
-                    config._save()
+                    with open (clean_up_log_folder, 'a+') as file2: 
+                        s=''
+                            
+                        folder_cnt=0
+                        file_cnt=0
+                        for subdir, dirs, files in walk(app_folder):
+                                for dir in dirs:
+                                    if find_with_list(dir,folder_list):
+                                        folder=join(subdir, dir)
+                                        rmtree(folder)
+                                        folder_cnt+=1
+                                        log_it('folder',folder,file2)
+
+                                for file in files:
+                                    if find_with_list(file,files_list):
+                                        file=join(subdir, file)
+                                        remove(file)
+                                        file_cnt+=1
+                                        log_it('file',file,file1)
+
+                cls._log.info(f'Removed {file_cnt} files and {folder_cnt} folders from {app_folder}')
+                # if count > 0:
+                #     config.application[app]['aip']=""
+                #     config._save()
 
         cls._log.debug('Source Code cleanup done')
 
+def log_it(typ,nm,fno):
+    s=f'Removed {typ} -> {nm})'
+    fno.write(s)
+    fno.write('\n') 
+
+def find_with_list(find_in:str,pattern:list):
+    rslt = False
+    for p in pattern:
+        if re.match(p,find_in):
+            rslt = True
+            break
+    return rslt
 
 class cleanUpHL(cleanUpAIP):
     def __init__(cls,config:Config, log_level:int):
