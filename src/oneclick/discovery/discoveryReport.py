2e3300ceee30939e3ccc2f9ead75821dd4e54817
Nevin Kaplan <n.kaplan@castsoftware.com>
2023-06-28 17:08:01 -0400

    discovery report issue corrections

--- src/oneclick/discovery/discoveryReport.py
+++ src/oneclick/discovery/discoveryReport.py
@@ -2,9 +2,12 @@ import pandas
 from oneclick.discovery.sourceValidation import SourceValidation 
 from pandas import DataFrame,read_excel
 from os.path import abspath
-import docx
 from oneclick.config import Config
 from cast_common.util import convert_LOC
+
+from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH,WD_LINE_SPACING
+from docx.shared import Length
 
 #todo: add totals to the cloc report (d1)
 #todo: if SQL problems, add bullet under SQL Delivery to describe (d1)
@@ -34,7 +37,7 @@ class DiscoveryReport(SourceValidation):
         cloc_report = abspath(f'{config.report}/{config.project_name}/{config.project_name}-cloc.xlsx')
         discovery_report = abspath(f'{config.report}/{config.project_name}/{config.project_name}-source-code-discovery.docx')
         # create an instance of a word document
-        doc = docx.Document()
+        doc = Document()
         # add a heading of level 0 (largest heading)
         doc.add_heading('Source code discovery report', 0)
         #doc_para = doc.add_paragraph(f'Please find below the completed discovery for the Project {config.project_name}')
@@ -51,15 +54,17 @@ class DiscoveryReport(SourceValidation):
 
             # read by 'Stats Before Code CleanUP' sheet of an Cloc_Output excel file
             before_df = cls.cloc_report(cloc_report,f'Before({appl})')
+            before_df=before_df[before_df['LANGUAGE'] != 'Totals']
+            before_df[before_df.columns]=before_df.apply(lambda x: x.str.strip() if isinstance(x, str) else x)
             #print(before_df)
 
             filt=(before_df['APPLICABLE']==False)
             l=before_df.loc[filt,['LANGUAGE','CODE']].sort_values(by=['CODE'], ascending=False) 
             non_code=''
             if len(l) == 1:
-                non_code= str(l.iloc[0]['CODE']//1000) +' KLOC of '+ l.iloc[0]['LANGUAGE']
+                str(l.iloc[0]['CODE']//1000) +' KLOC of '+ l.iloc[0]['LANGUAGE']
             elif len(l) > 1:
-                non_code= str(l.iloc[0]['CODE']//1000) +' KLOC of '+ l.iloc[0]['LANGUAGE'] +' non code files and ~'+ str(l.iloc[1]['CODE']//1000) +' KLOC of '+ l.iloc[1]['LANGUAGE']
+                non_code= str(l.iloc[0]['CODE']//1000) +' KLOC of '+ l.iloc[0]['LANGUAGE'].strip() +' non code files and ~'+ str(l.iloc[1]['CODE']//1000) +' KLOC of '+ l.iloc[1]['LANGUAGE']
 
             # read by 'Stats After Code CleanUP' sheet of an Cloc_Output excel file
             after_df = cls.cloc_report(cloc_report,f'After({appl})')
@@ -72,27 +77,31 @@ class DiscoveryReport(SourceValidation):
             doc.add_heading(f'Application {appl} :', 2)
 
             # out of scope code base
-            total = int(before_df['CODE'].sum())
-            total, unit = convert_LOC(total)
-            doc.add_paragraph(f"This delivery contains a total of {len(after_df)-1} technologies/extensions discovered with a total of {total} {unit}.",style='List Bullet')
+            langs=len(before_df)
+            total, unit = convert_LOC(int(before_df['CODE'].sum()))
+            doc.add_paragraph(f"This delivery contains {langs} discovered technolog{'ies' if langs > 1 else 'y'}/extension{'s' if langs > 1 else ''}  and a total of {total} {unit}.",style='List Bullet')
 
             bsuport = after_df[after_df['APPLICABLE']==True]
             total = int(bsuport['CODE'].sum())
             total, unit = convert_LOC(total)
 
-            lang_list = list(bsuport['LANGUAGE'])
-            lang_list[-1]=f'and {lang_list[-1]}'
+            lang_list = list(bsuport['LANGUAGE'].str.strip())
+            langs = len(lang_list)
+            if langs >1:
+                lang_list[-1]=f'and {lang_list[-1]}'
             bsup_lang = ', '.join(lang_list)
-            doc.add_paragraph(f"{len(bsuport)} technologies/extensions are relevant for the CAST analysis, which include {bsup_lang}.",style='List Bullet')
+            doc.add_paragraph(f"{len(bsuport)} technolog{'ies' if langs > 1 else 'y'}/extension{'s are' if langs > 1 else ' is'} relevant for the CAST analysis, which include {bsup_lang}.",style='List Bullet')
 
             nsuport = after_df[after_df['APPLICABLE']==False]
             total = int(bsuport['CODE'].sum())
             total, unit = convert_LOC(total)
 
-            lang_list = list(nsuport['LANGUAGE'])
-            lang_list[-1]=f'and {lang_list[-1]}'
+            lang_list = list(nsuport['LANGUAGE'].str.strip())
+            langs = len(lang_list)
+            if langs > 1:
+                lang_list[-1]=f'and {lang_list[-1]}'
             bsup_lang = ', '.join(lang_list)
-            doc.add_paragraph(f"The remaining {len(nsuport)} technologies/extensions are not relevant and will not be analyzed. These include {bsup_lang}.",style='List Bullet')
+            doc.add_paragraph(f"The remaining technolog{'ies' if langs > 1 else 'y'}/extension{'s are' if langs > 1 else ' is'} not relevant and will not be analyzed. These include {bsup_lang}.",style='List Bullet')
 
             # in scope code base
             # asuport = after_df[before_df['APPLICABLE']==True]
@@ -108,7 +117,7 @@ class DiscoveryReport(SourceValidation):
             sql_items=''
 
             if tables == 0 and procedures == 0 and functions == 0 and triggers == 0 and views == 0:
-                doc.add_paragraph(f'The source code does not contains SQL items.',style='List Bullet')
+                doc.add_paragraph(f'The source code does not contain any SQL items.',style='List Bullet')
             else:
                 if tables>0:
                     sql_items = sql_items + str(tables) +' tables, '
@@ -121,7 +130,7 @@ class DiscoveryReport(SourceValidation):
                 if views>0:
                     sql_items = sql_items + str(views) +' views '
                 
-                doc.add_paragraph(f'The database contains a total of '+sql_items+'.',style='List Bullet')
+                doc.add_paragraph(f'The database contains a total of {sql_items}.',style='List Bullet')
 
             # line = []
             # for index, row in sql_df.iterrows():
@@ -130,7 +139,7 @@ class DiscoveryReport(SourceValidation):
             #         line[-1]=f'in {line[-1]}'
             #doc.add_paragraph(f"The delivery also inclues, {', '.join(line)}.",style='List Bullet')
             removed_code=before_df['CODE'].sum()-after_df['CODE'].sum()
-            doc.add_paragraph(f'We removed {removed_code} KLOC of sample, test and other non-production code from the source code.',style='List Bullet')
+            doc.add_paragraph(f'We removed {round(removed_code/1000,1)} KLOC of sample, test and other non-production code from the source code.',style='List Bullet')
             after_df=after_df.drop(after_df[after_df['APPLICABLE']==0.0].index)
             total = after_df['CODE'].sum()//1000
             doc.add_paragraph(f"Here is a high-level summary of the source code that will be analyzed by CAST, totaling {total} KLOC.",style='List Bullet')
@@ -145,20 +154,32 @@ class DiscoveryReport(SourceValidation):
             t = doc.add_table(after_df.shape[0]+1, after_df.shape[1])
             # add the header rows.
             for j in range(after_df.shape[-1]):
-                t.cell(0,j).text = after_df.columns[j]
+                t.cell(0,j).text = after_df.columns[j].strip()
+                if j > 0:
+                    t.cell(0,j).paragraphs[0].alignment=WD_ALIGN_PARAGRAPH.RIGHT
 
             # add the rest of the data frame
             for i in range(after_df.shape[0]):
                 for j in range(after_df.shape[-1]):
                     number = after_df.values[i,j]
                     if type(number) is int:
-                        number = "{}".format(number)
+                        number = "{:,}".format(number)
                     t.cell(i+1,j).text = number
-            
+                    if j > 0:
+                        t.cell(i+1,j).paragraphs[0].alignment=WD_ALIGN_PARAGRAPH.RIGHT
+            make_rows_bold(t.rows[len(after_df)])
+
             # Adding style to a table
             t.style = 'Medium Shading 1 Accent 1'
 
         # now save the document to a location
         doc.save(discovery_report)
         cls._log.info(f'Source Code Discovery report has been written to {discovery_report}')
-    pass
+
+
+def make_rows_bold(*rows):
+    for row in rows:
+        for cell in row.cells:
+            for paragraph in cell.paragraphs:
+                for run in paragraph.runs:
+                    run.font.bold = True
