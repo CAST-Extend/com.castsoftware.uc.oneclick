8ed773ef83ca268b522c091b5a072d7c9ee2a94b
Nevin Kaplan <n.kaplan@castsoftware.com>
2023-07-17 13:46:04 -0400

    if windows os use subst for cloc path

--- src/oneclick/discovery/cloc.py
+++ src/oneclick/discovery/cloc.py
@@ -4,11 +4,17 @@ from cast_common.util import run_process,check_process,format_table,create_folde
 from oneclick.discovery.sourceValidation import SourceValidation 
 from time import sleep
 
-from os import getcwd
+from platform import system
 from os.path import exists,abspath,getsize
 from re import findall
 from pandas import DataFrame,ExcelWriter
 
+from string import ascii_uppercase
+from win32api import GetLogicalDriveStrings
+from win32wnet import WNetOpenEnum,WNetEnumResource
+from win32netcon import RESOURCE_REMEMBERED,RESOURCETYPE_DISK
+
+from ctypes import windll, c_int, c_wchar_p
 
 #TODO: Convert total line to formulas (d1-SHP)
 #TODO: Format all numbers as integers not text (d1-SHP)
@@ -42,8 +48,26 @@ class ClocPreCleanup(SourceValidation):
     def cloc_results(cls):
         return cls._df
 
+    def _get_free_drive(cls):
+        drives = set(ascii_uppercase[2:])
+        for d in GetLogicalDriveStrings().split(':\\\x00'):
+            drives.discard(d)
+        # Discard persistent network drives, even if not connected.
+        henum = WNetOpenEnum(RESOURCE_REMEMBERED, 
+            RESOURCETYPE_DISK, 0, None)
+        while True:
+            result = WNetEnumResource(henum)
+            if not result:
+                break
+            for r in result:
+                if len(r.lpLocalName) == 2 and r.lpLocalName[1] == ':':
+                    drives.discard(r.lpLocalName[0])
+        if drives:
+            return sorted(drives)[-1] + ':'
+
     def _run_cloc(cls,work_folder:str,cloc_output:str,cloc_output_ignored:str):
         args = [cls.cloc_path,work_folder,"--report-file",cloc_output,"--ignored",cloc_output_ignored,"--quiet"]
+        cls._log.info(' '.join(args))
         proc = run_process(args,False)
 
         sleep(10)
@@ -64,12 +88,28 @@ class ClocPreCleanup(SourceValidation):
             
         process = {}
         cloc_run=False
+
+        DefineDosDevice = windll.kernel32.DefineDosDeviceW
+        DefineDosDevice.argtypes = [ c_int, c_wchar_p, c_wchar_p ]
+
+        project_folder=abspath(f'{config.work}/AIP/{config.project_name}')
+        # Create a subst. Check the return for non-zero to mean success
+        drive=project_folder
+        platform = system()
+        if platform == 'Windows':
+            drive = cls._get_free_drive()
+            if DefineDosDevice(0, drive, project_folder ) == 0:
+                raise RuntimeError("Subst failed")
+
         for appl in config.application:
             cls._log.info(f'Running {config.project_name}/{appl}')
             create_folder(f'{config.report}/{config.project_name}/{appl}')
             cloc_output = abspath(f'{config.report}/{config.project_name}/{appl}/{appl}-cloc-{cls.phase}.txt')
             cloc_output_ignored = abspath(f'{config.report}/{config.project_name}/{appl}/{appl}-cloc-ignored-{cls.phase}.txt')
-            work_folder = abspath(f'{config.work}/AIP/{config.project_name}/{appl}')
+            if platform == 'Windows':            
+                work_folder = abspath(f'{drive}/{appl}')
+            else:
+                work_folder = abspath(f'{config.work}/AIP/{config.project_name}/{appl}')
 
             #if the report is already out there - no need to continue
             if exists(cloc_output):
@@ -103,29 +143,30 @@ class ClocPreCleanup(SourceValidation):
             if cloc_run:
                 sleep(60)
 
+        # Delete the subst.
+        if platform == 'Windows' and DefineDosDevice(2, drive, project_folder ) == 0:
+            raise RuntimeError("Subst failed")
+
         for appl in config.application:
             #reading cloc_output.txt file
             cloc_output = abspath(f'{config.report}/{config.project_name}/{appl}/{appl}-cloc-{cls.phase}.txt')
             cloc_output_ignored = abspath(f'{config.report}/{config.project_name}/{appl}/{appl}-cloc-ignored-{cls.phase}.txt') 
             cls._log.info(f'Processing {cloc_output}')
-            summary_list=[]   
             with open(cloc_output, 'r') as f:
                 content = f.read()
-                f.seek(0)
-                summary_list= [line.rstrip('\n').lstrip() for line in f]
-                #print(summary_list)
 
             #extracting required data from content of cloc_output.txt using python regex
-#            pattern='(\S{1,}|\w{1,}[:])\s{1,}(\d{1,})\s{1,}(\d{1,})\s{1,}(\d{1,})\s{1,}(\d{1,})'
-            pattern='([\w #]{30})\s{1,}(\d{1,})\s{1,}(\d{1,})\s{1,}(\d{1,})\s{1,}(\d{1,})'
-            statistics_list=findall(pattern,content)
-            statistics_list= statistics_list[:-1]
+            header=content.split('\n')[2]
+            header_list=findall('\w+',header.upper())
+
+            summary='\n'.join(content.split('\n')[4:-4])
+            pattern='(.{25})\s{1,}(\d{1,})\s{1,}(\d{1,})\s{1,}(\d{1,})\s{1,}(\d{1,})'
+            statistics_list=findall(pattern,summary)
             
             with open(cloc_output_ignored, 'r') as fp:
                 lines = len(fp.readlines())
             statistics_list.append(('Unknown Files','0','0','0',str(lines)))
-
-            df = DataFrame(statistics_list,columns=['LANGUAGE','FILES','BLANK','COMMENT','CODE'])
+            df = DataFrame(statistics_list,columns=header_list)
 
             #making technolgy case insensitive
             tech_list = list(map(lambda x: x.lower().strip(), tech_list))
