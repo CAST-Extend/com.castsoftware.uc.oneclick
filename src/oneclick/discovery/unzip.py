922da83d6fbeec3f2d2c8f97d801a09518635e8d
Nevin Kaplan <n.kaplan@castsoftware.com>
2023-08-31 16:46:05 -0400

    add new zip functionality

--- src/oneclick/discovery/unzip.py
+++ src/oneclick/discovery/unzip.py
@@ -20,41 +20,46 @@ class Unzip(SourceValidation):
 
    def unzip(cls,src_fldr) -> bool:
       error = False
-      found = False
-      for root, dirs, files in walk(src_fldr):
-         try:
-            if '__MACOSX' in root or '.DS_Store' in files:
-               continue
-            for file in files:
-               full_name = abspath(f'{root.strip()}\\{file}')
-               dest = full_name.replace(f".{full_name.split('.')[-1]}",'')
-
-               found=False
-               if file.endswith(".gz"):
-                  found = True
-                  with gz(full_name, 'rb') as f_in:
-                     if not exists(dest):
-                        with open(dest, 'wb') as f_out:
-                           copyfileobj(f_in, f_out)               
-
-               elif file.endswith(".tar") or file.endswith(".gztar") or file.endswith(".bztar"):
-                  with tar(full_name) as f_in:
-                     found = True
-                     create_folder(dest)
-                     f_in.extractall(dest)
-
-               elif file.endswith(".7z") or file.endswith(".zip") :
-                  found = True
+      found = True
+      while found:
+         for root, dirs, files in walk(src_fldr):
+            try:
+               if '__MACOSX' in root or '.DS_Store' in files:
+                  continue
+               for file in files:
                   full_name = abspath(f'{root.strip()}\\{file}')
-                  Archive(full_name).extractall(dest,auto_create_dir=True)
+                  dest = full_name.replace(f".{full_name.split('.')[-1]}",'')
 
-               if found:     
-                  cls._log.info(f'Unpacked: {full_name}')  
-                  remove(full_name)
+                  found=False
+                  if file.endswith(".gz"):
+                     found = True
+                     with gz(full_name, 'rb') as f_in:
+                        if not exists(dest):
+                           with open(dest, 'wb') as f_out:
+                              copyfileobj(f_in, f_out)               
 
-         except (TarError, ValueError, Exception) as ex:
-            cls._log.error(str(ex))
-            error = True
+                  elif file.endswith(".tar") or \
+                       file.endswith(".gztar") or \
+                       file.endswith(".bztar") or \
+                       file.endswith(".tgz"):
+                     with tar(full_name) as f_in:
+                        found = True
+                        create_folder(dest)
+                        f_in.extractall(dest)
+
+                  elif file.endswith(".7z") or file.endswith(".zip") :
+                     found = True
+                     full_name = abspath(f'{root.strip()}\\{file}')
+                     Archive(full_name).extractall(dest,auto_create_dir=True)
+
+                  if found:     
+                     cls._log.info(f'Unpacked: {full_name}')  
+                     remove(full_name)
+
+            except (TarError, ValueError, Exception) as ex:
+               cls._log.error(f'Error while unpacking: {full_name}')  
+               cls._log.error(str(ex))
+               error = True
 
       #terminate on error 
       if error: 
