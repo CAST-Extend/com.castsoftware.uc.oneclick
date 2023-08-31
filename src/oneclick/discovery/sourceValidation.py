33033fb41f25f16041ecac0082d20ebc149d16b3
Nevin Kaplan <n.kaplan@castsoftware.com>
2023-08-31 16:44:54 -0400

    update logging configuration

--- src/oneclick/discovery/sourceValidation.py
+++ src/oneclick/discovery/sourceValidation.py
@@ -6,8 +6,8 @@ from os.path import abspath
 class SourceValidation:
 
     def __init__(cls,config:Config,log_name:str,log_level:int):
-        log_file = abspath(f'{config.oneclick_work}/{config.project_name}/LOGS/source-code-discovery.log')       
-        cls._log = Logger(name=log_name,level=log_level,file_name=log_file)
+#        log_file = abspath(f'{config.oneclick_work}/{config.project_name}/LOGS/source-code-discovery.log')       
+        cls._log = Logger(name=log_name,level=log_level,file_name=config.log_filename)
         pass
 
     @property
