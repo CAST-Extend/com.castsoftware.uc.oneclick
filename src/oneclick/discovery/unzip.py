aaf918837d3b2fa6dba6974879a1538d369b501d
sharath9620 <s.p@castsoftware.com>
2024-02-26 19:09:12 +0530

    discovery_logs

--- src/oneclick/discovery/unzip.py
+++ src/oneclick/discovery/unzip.py
@@ -23,6 +23,8 @@ class Unzip(SourceValidation):
         super().__init__(config,cls.__class__.__name__,log_level)
 
    def unzip(cls,src_fldr) -> bool:
+      unarchived_files = 0
+      archived_files = 0
       error = False
       found = True
       while found:
@@ -37,6 +39,7 @@ class Unzip(SourceValidation):
 
                   found=False
                   if file.endswith(".gz"):
+                     archived_files += 1
                      found = True
                      with gz(full_name, 'rb') as f_in:
                         if not exists(dest):
@@ -48,21 +51,27 @@ class Unzip(SourceValidation):
                        file.endswith(".bztar") or \
                        file.endswith(".tgz"):
                      with tar(full_name) as f_in:
+                        archived_files += 1
                         found = True
                         create_folder(dest)
                         f_in.extractall(dest)
 
                   elif file.endswith(".7z") or file.endswith(".zip") :
+                     archived_files += 1
                      found = True
                      full_name = abspath(f'{root.strip()}\\{file}')
                      create_folder(dest)
                      with ZipFile(full_name) as zf:
-                        for member in tqdm(zf.infolist(), desc='Extracting ',position=0,leave=False):
+                        # for member in tqdm(zf.infolist(), desc='Extracting ',position=0,leave=False):
+                        for member in zf.infolist():
                            zf.extract(member, dest)
                      #Archive(full_name).extractall(dest,auto_create_dir=True)
 
+                  else:
+                     unarchived_files += 1
+
                   if found:     
-                     cls.zip_log.info(f'Unpacked: {full_name}')  
+                     # cls.zip_log.info(f'Unpacked: {full_name}')  
                      remove(full_name)
             except BadZipFile:
                #not a valid zip file log it then remove from staging folder
@@ -80,10 +89,10 @@ class Unzip(SourceValidation):
       if error: 
          raise
 
-      return found
+      return found, archived_files, unarchived_files
 
    def run(cls,config:Config):
-      cls._log.info(f'Running {cls.__class__.__name__} for all applications')
+      # cls._log.info(f'Running {cls.__class__.__name__} for all applications')
       cls._log.debug('Running unzip step')
 
       #scan delivery folder for application folders
@@ -95,7 +104,8 @@ class Unzip(SourceValidation):
       found = True
       while found:
          found = False
-         for app in tqdm(apps, desc="Unzipping files", leave=True):
+         # for app in tqdm(apps, desc="Unzipping files", leave=True):
+         for app in apps:
             log_folder=abspath(f'{config.oneclick_work}/LOGS/{config.project_name}/{app}')
             create_folder(log_folder)
 
@@ -106,8 +116,15 @@ class Unzip(SourceValidation):
             log_name = 'Unzip-files'
             cls.zip_log = Logger(log_name,level=cls._log_level,file_name=zip_log_file,console_output=True)
 
-            found = cls.unzip(abspath(f'{config.work}\\AIP\\{config.project_name}\\{app}'))
-            found = cls.unzip(abspath(f'{config.work}\\HL\\{config.project_name}\\{app}'))
+            found, archived_files, unarchived_files = cls.unzip(abspath(f'{config.work}\\AIP\\{config.project_name}\\{app}'))
+            found, archived_files, unarchived_files = cls.unzip(abspath(f'{config.work}\\HL\\{config.project_name}\\{app}'))
+
+            cls._log.info(f'application {app}, found {unarchived_files} files and {archived_files} archive files')
+            embedded_archives = archived_files - 1
+            if embedded_archives > 0:   
+               cls._log.info(f'{archived_files} files unzipped successfully. {embedded_archives} embedded archives found and unzipped successfully.\n')
+            else:
+               cls._log.info(f'{archived_files} files unzipped successfully. 0 embedded archives found.\n')
 
             Logger.loggers.remove(log_name)
 
@@ -119,8 +136,10 @@ class Unzip(SourceValidation):
                      
             #          remove(full_name)
 
-      cls._log.info('Unzip step complete')
-                                
+      # cls._log.info('Unzip step complete')
+
+   def get_title(cls) -> str:
+      return 'DISCOVER ARCHIVES AND UNZIP\n'                               
 
 
 
